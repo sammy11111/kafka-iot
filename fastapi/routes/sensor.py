f320cb970dcb836af47c804bd77fced11e2f2fda
Sammy Ibrahim <engineero73@gmail.com>
2025-04-02 15:27:22 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ fastapi/routes/sensor.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, HTTPException
+from app.schemas.sensor import SensorData
+from app.services.kafka_producer import produce_to_kafka
+
+
+sensor_router = APIRouter()
+
+@sensor_router.post("/")
+async def post_sensor_data(sensor_data: SensorData):
+    try:
+        produce_to_kafka(sensor_data.dict())
+        return {"status": "success"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
